1a64b95255907dd1b039bbadbffb80033dba14f1
arct0r <109032203+arct0r@users.noreply.github.com>
2024-06-16 17:06:12 +0200

    created a very simple streamlit interface with 3 pages

---
+++ pages/Chat.py
@@ -0,0 +1,54 @@
+import streamlit as st
+import random
+import time
+st.set_page_config(
+    page_title="Chat",
+    page_icon="💬",
+)
+
+
+st.title('Interface for the Redis Chat')
+
+# Tutorial
+# https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps#build-a-simple-chatbot-gui-with-streaming
+
+
+with st.sidebar:
+    friends=['AI', 'Hoomanz']
+    selection = st.selectbox(label='Select who you wanna chat with.', options=friends)
+
+# Streamed response emulator
+def response_generator():
+    response = random.choice(
+        [
+            f"***{selection}:*** Hello there! How can I assist you today?",
+            f"***{selection}:*** Hi, human! Is there anything I can help you with?",
+            f"***{selection}:*** Do you need help?",
+        ]
+    )
+    for word in response.split():
+        yield word + " "
+        time.sleep(0.05)
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown("***User:*** "+prompt)
+
+    # Display assistant response in chat message container
+    with st.chat_message(selection):
+        response = st.write_stream(response_generator())
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": selection, "content": response})
