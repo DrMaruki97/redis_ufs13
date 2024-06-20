e35ffcc6f2f1b92b80276dab1925000d22c5de89
arct0r <109032203+arct0r@users.noreply.github.com>
2024-06-20 14:29:55 +0200

    ooops wrong server

--- conn.py
+++ conn.py
@@ -2,8 +2,9 @@ import redis
 
 def connect():
     r = redis.Redis(
-        host='redis-10033.c328.europe-west3-1.gce.redns.redis-cloud.com',
-        port=10033,
-        password='kvgK4FP0P2HkKBsl2Vt5Odc2abp1e2mb',
-        decode_response=True)
+    host='redis-16230.c328.europe-west3-1.gce.redns.redis-cloud.com',
+    port=16230,
+    password='y6ORUWqEjBvQZU3ICfuV8dgU8glOYFwL',
+    decode_responses=True
+    )
     return r
