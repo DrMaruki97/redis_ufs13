a8ea0ab3dfaec307cfd6d4a7b0546c6e82d7284f
DrMaruki97 <luca.giovagnoli1997@outlook.it>
2024-06-22 13:59:44 +0200

    display chat e presa in carico messaggio

--- ui_functions.py
+++ ui_functions.py
@@ -1,4 +1,5 @@
-import redis
+import datetime as dt
+import StreamMethods as sm
 
 def Page(page):
     print(intestazione)
@@ -16,4 +17,20 @@ def chats(diz):
     for i in range(len(lista)):
         print(f'{i+1}  {lista[i]}')
     return lista
-    
+
+
+def msgs(user,messaggi:list):
+    if messaggi:
+        for el in messaggi:
+            msg = el[1]
+            if msg['mittente'] == user:
+                mitt = '>'
+            else:
+                mitt = '<'
+            print(f'{mitt} {msg['messaggio']}\t{msg['datetime']}')
+
+
+def speak(user):
+    messaggio = input('>> ')
+    date = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    return {'mittente':user,'messaggio':messaggio,'datetime':date}
