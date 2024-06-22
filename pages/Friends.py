8b0aaa7026ba054b69f455eec74ecb550ed38b90
arct0r <109032203+arct0r@users.noreply.github.com>
2024-06-22 21:15:18 +0200

    You can now add friends and remove them in bulk.

--- pages/Friends.py
+++ pages/Friends.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from Login import streamlit_logout
+import pandas as pd
 
 if 'user' in st.session_state:
-
     st.title('Friends')
     st.sidebar.text(f"Currently logged in as {st.session_state['user']}")
     if st.session_state['status'] == '1':
@@ -18,19 +18,40 @@ else:
     st.info('Please Login from the Home page and try again.')
     st.switch_page('Login.py')
 
-friends = []
+friends = st.session_state.r.hgetall(f"st:friendList:{st.session_state.user}")
+friends_df = pd.DataFrame({'User':friends.keys() for friend in friends})
+friends_df['Remove'] = [False for friend in friends]
 
 if not friends:
     st.write('***Your current friendlist:***')
     'It seems like you have no friends, just like in real life.'
-    st.image(image='pages/pepecry.png')
+    st.image(image='pages/pepecry.gif')
 else:
-    friends
+    friends_df = st.data_editor(friends_df, hide_index=True)
+    conf_remove = st.button(label='Remove from friendlist')
+    friends_to_be_removed = [user for user in friends_df[friends_df['Remove']==True]['User']]
+    if conf_remove and friends_to_be_removed:
+        st.session_state.r.hdel(f"st:friendList:{st.session_state.user}", *friends_to_be_removed)
+        # Che comando! Semplicemente deleta dall'hash table (la friendlist) dell'user loggato gli amici che sull'interfaccia sono marchiati con una X
+        st.success('Removed your buddies.')
+
+
 
 
 st.divider()
 st.title('Add a friend')
 friend = st.text_input(label='Type your friend username')
+add_button = st.button(label='Add a friend')
+if add_button:
+    if st.session_state.r.exists('user:'+friend):
+        st.session_state.r.hset(f"st:friendList:{st.session_state.user}", friend, 'temp')
+        friends = st.session_state.r.hgetall(f"st:friendList:{st.session_state.user}")
+        friends_df = pd.DataFrame({'User':friends.keys() for friend in friends})
+        st.success(f'Added {friend}')
+    else: 
+        st.error("User simply ain't there buddy")
+
+
 
 
 
