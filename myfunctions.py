77929004b6ea7bc0cb66623a553362bce1622fbe
DrMaruki97 <luca.giovagnoli1997@outlook.it>
2024-06-23 13:06:40 +0200

    funzioni di test

--- myfunctions.py
+++ myfunctions.py
@@ -1,29 +1,47 @@
 import redis
-import functions as f
 
-def login(user,psw):
-    if f.hash_pwd(psw) == r.get(user.lower()):
+
+
+def login(user,psw,r):
+    if psw == r.get(user.lower()):
         return True
     return False
 
 
-def registration(user,psw):
+def registration(user,psw,r):
 
     try:
-        r.set(user.lower(),f.hash_pwd(psw))
+        r.set(user.lower(),psw)
         r.set(f'DnD:{user}',0)
         return True
     except:
         return False
     
 
-def check_disp(user):
+def check_disp(user,r):
     value = r.get(user.lower())
-    if value:
+    if not value:
         return True
     return False
 
 
 def check_psw(psw):
-    return 4<=psw<=16
+    return 4<=len(psw)<=16
+
+
+def user_serch(pattern,r):
+    users = r.scan(match = f'{pattern}*')[1]
+    return users
+
+
+def add_friend(user,friend,r):
+    r.hset(f'User:{user}',friend,0)
+
+
+def get_friends(user,r):
+    friends = r.get(f'User:{user}')
+    lista = list(friends.keys())
+    return lista
+
+
         
